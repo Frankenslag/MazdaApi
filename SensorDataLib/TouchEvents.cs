3a7bab4660876d2137632154bac16cd477af908f
Andrew Gould <andrew@wingandprayer.co.uk>
2021-07-29 10:00:46 +0100

    Added Headers

--- SensorDataLib/TouchEvents.cs
+++ SensorDataLib/TouchEvents.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿// //
+// // TouchEvents.cs
+// //
+// // Copyright 2021 Wingandprayer Software
+// //
+// // This file is part of MazdaApiLib.
+// //
+// // MazdaApiLib is free software: you can redistribute it and/or modify it under the terms of the
+// // GNU General Public License as published by the Free Software Foundation, either version 2
+// // of the License, or (at your option) any later version.
+// //
+// // CDRipper is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// // PURPOSE. See the GNU General Public License for more details.
+// //
+// // You should have received a copy of the GNU General Public License along with MazdaApiLib.
+// // If not, see http://www.gnu.org/licenses/.
+// //
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
